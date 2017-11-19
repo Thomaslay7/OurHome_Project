dc9b771242f89e5c9d7f8608115a45062b2f7497
thomaslay7 <merrydicky83@yahoo.co.id>
2017-11-19 21:04:13 +0700

    gui

---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject login,create;
+
+	void Start () {
+		login.transform.localScale = new Vector3(0f,0f,0f);
+		create.transform.localScale = new Vector3(0f,0f,0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void showLogin()
+	{
+		
+		login.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+		Debug.Log ("check login");
+	}
+	public void showCreateFamily()
+	{
+
+		create.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+		Debug.Log ("check create family");
+	}
+}
