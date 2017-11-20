32b83a3d710e148b98725a352bbe0fd64eb24a79
thomaslay7 <merrydicky83@yahoo.co.id>
2017-11-20 15:41:52 +0700

    gui2

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class UIManager : MonoBehaviour {
 
 	// Use this for initialization
-	public GameObject login,create,createprofil,choosetasks,choosegroceries,back,add, family/*,next,groceries,tasks,family,rewards,calendar*/;
+	public GameObject login,create,createprofil,choosetasks,choosegroceries,back,adds, family, groceries,tasks,rewards,calendar;
+	public GameObject btnNext,btnBack;
 
 	void Start () {
 		login.transform.localScale = Vector3.zero;
@@ -14,36 +15,53 @@ public class UIManager : MonoBehaviour {
 		createprofil.transform.localScale =  Vector3.zero;
 		choosetasks.transform.localScale = Vector3.zero;
 		choosegroceries.transform.localScale =  Vector3.zero;
-		add.transform.localScale =  Vector3.zero;
+		adds.transform.localScale =  Vector3.zero;
+		groceries.transform.localScale = Vector3.zero;
+		tasks.transform.localScale = Vector3.zero;
+		calendar.transform.localScale = Vector3.zero;
+		rewards.transform.localScale = Vector3.zero;
+		groceries.transform.localScale = Vector3.zero;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void HideAll()
+	{
+		login.transform.localScale = Vector3.zero;
+		create.transform.localScale = Vector3.zero;
+		family.transform.localScale = Vector3.zero;
+		createprofil.transform.localScale =  Vector3.zero;
+		choosetasks.transform.localScale = Vector3.zero;
+		choosegroceries.transform.localScale =  Vector3.zero;
+		adds.transform.localScale =  Vector3.zero;
+		groceries.transform.localScale = Vector3.zero;
+		tasks.transform.localScale = Vector3.zero;
+		calendar.transform.localScale = Vector3.zero;
+		rewards.transform.localScale = Vector3.zero;
+		groceries.transform.localScale = Vector3.zero;
 	}
 	public void showOurHome()
 	{
-
+		HideAll ();
 		back.transform.parent.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
 		Debug.Log ("check back");
 
 	}
 	public void showLogin()
 	{
-		
+		HideAll ();
 		login.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 		Debug.Log ("check login");
 
 	}
 	public void showCreateFamily()
 	{
-
+		HideAll();
 		create.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 		Debug.Log ("check create family");
 	}
 
 	public void showcreateprofile()
 	{
+		HideAll();
 
 		createprofil.transform.localScale = Vector3.one;
 		Debug.Log ("check create profile");
@@ -57,25 +75,66 @@ public class UIManager : MonoBehaviour {
 	public void showchoosetask()
 	{
 
+		HideAll ();
 		choosetasks.transform.localScale = Vector3.one;
 		Debug.Log ("check next TASK");
 	}
 	public void showchoosegroceries()
 	{
 
+		HideAll ();
 		choosegroceries.transform.localScale = Vector3.one;
 		Debug.Log ("check next");
 	}
 	public void showAdd()
 	{
 
-		add.transform.localScale = Vector3.one;
+		HideAll ();
+		adds.transform.localScale = Vector3.one;
 		Debug.Log ("check next");
 	}
 	public void showFamily()
 	{
 
+		HideAll ();
 		family.transform.localScale = Vector3.one;
+		Debug.Log ("check family");
+	}
+	public void showCalendar()
+	{
+
+		HideAll ();
+		calendar.transform.localScale = Vector3.one;
+		Debug.Log ("check calendar family");
+	}
+	public void showTasks()
+	{
+
+		HideAll ();
+		tasks.transform.localScale = Vector3.one;
+		Debug.Log ("check family tasks");
+	}
+	public void showRewards()
+	{
+
+		HideAll ();
+		rewards.transform.localScale = Vector3.one;
+		Debug.Log ("check family rewards");
+	}
+	public void showGroceries()
+	{
+
+		HideAll ();
+		groceries.transform.localScale = Vector3.one;
+		Debug.Log ("check family tasks");
+	}
+	public void showAddProfilFamily()
+	{
+
+		HideAll ();
+		btnBack.transform.localScale=Vector3.zero;
+		btnNext.transform.localScale=Vector3.zero;
+		createprofil.transform.localScale = Vector3.one;
 		Debug.Log ("check family");
 	}
 }
